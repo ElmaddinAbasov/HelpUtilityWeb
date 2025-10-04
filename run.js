eef32789918e44b08d0941fb81870f3478674a42
MichaelMyers0 <siracusa.augusta@gmail.com>
2025-10-04 16:48:59 +0300

    eighteens commit

--- run.js
+++ run.js
@@ -1,88 +1,89 @@
 "use strict"
-const starterData = ["Книги", "Eloquent JavaScript", "JavaScript: The Definitive Guide", "Learning JavaScript Design Patterns",
-			"Effective JavaScript", "Документация", "https://developer.mozilla.org", "https://262.ecma-international.or                         g/6.0/", "туториалы", "https://www.w3schools.com/js/", "https://javascript.info/"];
 
-const glossaryData = ["await", "break", "case", "catch", "class", "const", "continue", "default", "delete", "do",
-                	"else", "enum", "export", "extends", "false", "finally","for", "function", "if", "import", "in", "instanceof", "let", "new"];
+const starterData = [
+        ["Книги : ", " "], ["Eloquent JavaScript : ", `<a href="https://eloquentjavascript.net/">Eloquent JavaScript</a>`],
+        ["JavaScript: The Definitive Guide : ", "<a href=\"https://pepa.holla.cz/wp-content/uploads/2016/08/JavaScript-The-Definitive-Guide-6th-Edition.pdf\">JavaScript: The Definitive Guide</a>"], ["Learning JavaScript Design Patterns : ", "<a href=\"https://cdn.bookey.app/files/pdf/book/en/learning-javascript-design-patterns.pdf\">Learning JavaScript Design Patterns</a><br>"],
+        ["Документация : ", ""], ["Официальный сайт веб-разработки : ", "<a href=\"https://developer.mozilla.org\">Официальный сайт Mozilla</a>"]
+                ];
 
-const usageData = ["await\n async function f4() { try {\n const z = await Promise.reject(new Error(rejected!));\n} catch (e) {\nconsole.error(e); // Error: rejected!}\n}\n", "break\nfor (let i = 0; i < 10; i++) {  \nif (i === 3) { break; }\ntext += The number is  + i + <br>;}"];
+const glossaryData = [
+                ["await : ", "Оператор await используется для ожидания окончания Promise. Может быть использован только <br>внутри async function или на верхнем уровне модуля.<br><br>"], ["break : ", "Оператор break прерывает выполнение текущего цикла, оператора множественного выбора switch или блочного выражения с меткой. <br>Выполнение кода продолжается с конструкции, следующей за прерванной.<br><br>"], ["switch : ", "Инструкция switch сравнивает выражение со случаями, перечисленными внутри неё, а затем выполняет соответствующие инструкции.<br><br>"], ["try..catch : ", "Конструкция try...catch пытается выполнить инструкции в блоке try, и, в случае ошибки, выполняет блок catch.<br><br>"], ["class : ", "Class expression это способ определения класса в ECMAScript 2015 (ES6). Схожий с function expressions, class expressions <br>может быть именованным либо не иметь имени. Если он именованный, то его имя доступно только внутри класса.<br> JavaScript классы используют прототипно-ориентирование наследование.<br><br>"], ["const : ", "Значение констант не может быть изменено новым присваиванием, а также не может быть переопределено. <br>Константы (const) подчиняются области видимости уровня блока так же, как переменные, объявленные с использованием ключевого слова let.<br><br>"], ["continue : ", "Инструкция continue прерывает выполнение текущей итерации текущего или отмеченного цикла, и продолжает его выполнение на следующей итерации."]
+                    ];
+
+
+const usageData = [
+        ["await ожидает разрешения Promise и возвращает полученное значение.<br><br>", "<img src=\"await.png\" style=\"width=500px;height:500px\">"]];
+
 
 class helpModel
 {
-	#dataSet;
-	#delayTime;
-	#output;
-	constructor()
-	{
-		this.#dataSet = new Set();
-		this.#delayTime = 6000;
-		this.#output = document.getElementById("output");
-	}
-	dataSetInit(data)
-	{
-		for (const e of data)
-			this.#dataSet.add(e);
-	}
+        #data;
+        #delayTime;
+        #output;
+        constructor(iterableData)
+        {
+                this.#data = new Map(iterableData);
+                this.#delayTime = 15000;
+                this.#output = document.getElementById("output");
+        }
         run(heading)
         {
                 let content = heading;
-                for (const item of this.#dataSet)
-                        content += `<p>${item}</p>`;
+                for (const [key, value] of this.#data.entries())
+                        content += `<pre>${key} ${value}</pre>`;
                 this.#output.innerHTML = content;
                 setTimeout(() => {
                         this.#output.style.display = "none";
                 }, this.#delayTime)
                 this.#output.style.display = "";
         }
-	representAsATable(heading)
-	{
-		let content = heading, i;
-		content += '<table><tr>';
-		i = 0;
-		for (const item of this.#dataSet)
-		{
-			content += `<td>${item}</td>`;
-			i++;
-			if (i % 2 === 0)
-			{
-				content += '</tr><tr>';
-				continue;
-			}
-		}
-		content += '</table>';
-		this.#output.innerHTML = content;
+        representAsATable(heading)
+        {
+                let content = heading, i;
+                content += '<table><tr>';
+                i = 0;
+                for (const item of this.#data)
+                {
+                        content += `<td>${item}</td>`;
+                        i++;
+                        if (i % 2 === 0)
+                        {
+                                content += '</tr><tr>';
+                                continue;
+                        }
+                }
+                content += '</table>';
+                this.#output.innerHTML = content;
                 setTimeout(() => {
                         this.#output.style.display = "none";
                 }, this.#delayTime)
-                this.#output.style.display = "";			
-	}
+                this.#output.style.display = "";
+        }
 };
 
-let starterButton = document.getElementById("starterButton");
-let glossaryButton = document.getElementById("glossaryButton");
-let usageButton = document.getElementById("usageButton");
+const starterButton = document.getElementById("starterButton");
+const glossaryButton = document.getElementById("glossaryButton");
+const usageButton = document.getElementById("usageButton");
 
-
-let starter = new helpModel();
-starter.dataSetInit(starterData);
-
-let glossary = new helpModel();
-glossary.dataSetInit(glossaryData);
-
-let usage = new helpModel();
-usage.dataSetInit(usageData);
+const starter = new helpModel(starterData);
 
 function runStarter()
 {
-	starterButton.addEventListener('click', starter.run('<h2>Список книг, туториалов и ссылки на официальную документацию: </h2><br>'));
+        starterButton.addEventListener('click', starter.run('<h2>Список книг, туториалов и ссылки на официальную документацию: </h2><br>'));
 }
+
+const glossary = new helpModel(glossaryData);
 
 function runGlossary()
 {
-	glossaryButton.addEventListener('click', glossary.representAsATable('<h2>Список ключевых слов используемых языком JavaScript: </h2><br>'));
+        glossaryButton.addEventListener('click', glossary.run('<h2>Список ключевых слов используемых языком JavaScript: </h2><br>'));
 }
+
+const usage = new helpModel(usageData);
+
 
 function runUsage()
 {
-	usageButton.addEventListener('click', usage.run('<h2>Примеры использования ключевых слов и конструкций языка JavaScript: </h2>'));
+        usageButton.addEventListener('click', usage.run('<h2>Примеры использования ключевых слов и конструкций языка JavaScript: </h2>'));
 }
+
